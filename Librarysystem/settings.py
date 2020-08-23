5e28694c3ceaca5fbacc7caa66bde4ef55b6e0e8
Ammybells <ammybells@gmail.com>
2020-08-23 10:36:23 +0100

    new changes

--- Librarysystem/settings.py
+++ Librarysystem/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4*e(_s2joso0h(%w7g7(1irj(xa#==&6wav(41x*ujpq7psrn0'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
